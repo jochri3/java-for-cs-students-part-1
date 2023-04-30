bc3434a443ade9fbb683a4949ccf8596115cd7a3
Christian Lisangola <jochri3@gmail.com>
2023-05-01 01:17:56 +0300

    7.3.code

---
+++ 7.conditions/7_3_code.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+public class Program12ConditionsIntro {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Your age : ");
+        int age=input.nextInt();
+        if(age>=18){
+            System.out.println("You can vote");
+        }else{
+            System.out.println("You can't vote");
+        }
+    }
+}

373ca10dc0364b023b54af6582ba9146d7459318
Christian Lisangola <jochri3@gmail.com>
2023-05-01 00:53:04 +0300

    6.3.code

---
+++ 6.Keyboard input/6_3_code.java
@@ -0,0 +1,12 @@
+Scanner input=new Scanner(System.in);
+System.out.print("Enter your full name : ");
+ 
+//String fullName=input.next();
+//System.out.println(fullName);
+ 
+String firstName=input.next();
+String lastName=input.next();
+//System.out.println(firstName+" "+lastName);
+ 
+String middleName=input.next();
+System.out.println(firstName+" "+lastName+" "+middleName);

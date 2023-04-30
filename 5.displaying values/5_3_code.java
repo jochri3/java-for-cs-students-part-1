bed2dac6dcf5cfbff1ffacaa3c32cb1ef50b7586
Christian Lisangola <jochri3@gmail.com>
2023-05-01 00:36:18 +0300

    5.3. Code

---
+++ 5.displaying values/5_3_code.java
@@ -0,0 +1,7 @@
+String firstName="Chris";
+String lastName="Lisa";
+String fullName=firstName+" "+lastName;
+ 
+//System.out.println(firstName+lastName);
+//System.out.println(firstName+" "+lastName);
+System.out.println(fullName);

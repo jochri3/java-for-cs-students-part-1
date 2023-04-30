0fa06ecba31e631db6ca31c4d313c1ab5b4b1d8c
Christian Lisangola <jochri3@gmail.com>
2023-05-01 00:16:21 +0300

    4.2 code

---
+++ 4.basic operation on data types/4_2_code.java
@@ -0,0 +1,14 @@
+double length=500;
+double width=250;
+ 
+double perimeter=2*(length+width);
+double area=length*width;
+ 
+length=10;
+width=5;
+ 
+perimeter=2*(length+width);
+area=length*width;
+ 
+System.out.println(perimeter);
+System.out.println(area);

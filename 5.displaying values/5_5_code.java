90526d5956c38e3273a520554d606b23ce0d6892
Christian Lisangola <jochri3@gmail.com>
2023-05-01 00:44:00 +0300

    5.5. Code

---
+++ 5.displaying values/5_5_code.java
@@ -0,0 +1,8 @@
+String firstName="Chris";
+String lastName="Lisa";
+int age=45;
+double weight=78;
+ 
+//I'm Chris Lisa, 45 years old, and I weigh 78 kg
+System.out.println("I'm "+ firstName +" "+lastName+", "+age+" years old, and I weigh "+weight+" kg.");
+System.out.printf("I'm %s %s, %d years old, and I weigh %.2f kg.",firstName,lastName,age,weight);

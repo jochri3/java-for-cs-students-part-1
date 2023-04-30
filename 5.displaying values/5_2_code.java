4b295b3c6190f2fdbed07998b8e758aa929c2e1c
Christian Lisangola <jochri3@gmail.com>
2023-05-01 00:33:19 +0300

    5.2. Code

---
+++ 5.displaying values/5_2_code.java
@@ -0,0 +1,12 @@
+int number1=50;
+int number2=30;
+ 
+int sum=number1+number2;
+int multi=number1*number2;
+int difference=number1-number2;
+double ratio = (double) number1/(double) number2;
+ 
+System.out.println(number1+" + "+number2+" = "+sum);
+System.out.println(number1+" x "+number2+" = "+multi);
+System.out.println(number1+" - "+number2+" = "+difference);
+System.out.println(number1+" / "+number2+" = "+ratio);

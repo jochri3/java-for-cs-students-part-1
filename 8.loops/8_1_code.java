97ffc025d91fe3d50f0466438b6be54bfb767166
Christian Lisangola <jochri3@gmail.com>
2023-05-01 09:30:04 +0300

    8.1.code

---
+++ 8.loops/8_1_code.java
@@ -0,0 +1,15 @@
+public class Program14LoopsIntro {
+    public static void main(String[] args) {
+        System.out.printf("The square of 2 is %.0f\n", Math.pow(2, 2));
+        System.out.printf("The square of 3 is %.0f\n", Math.pow(3, 2));
+        System.out.printf("The square of 4 is %.0f\n", Math.pow(4, 2));
+        System.out.printf("The square of 5 is %.0f\n", Math.pow(5, 2));
+        System.out.printf("The square of 6 is %.0f\n", Math.pow(6, 2));
+        System.out.printf("The square of 7 is %.0f\n", Math.pow(7, 2));
+        System.out.printf("The square of 8 is %.0f\n", Math.pow(8, 2));
+        System.out.printf("The square of 9 is %.0f\n", Math.pow(9, 2));
+        System.out.printf("The square of 10 is %.0f\n", Math.pow(10, 2));
+        System.out.printf("The square of 11 is %.0f\n", Math.pow(11, 2));
+        System.out.printf("The square of 12 is %.0f\n", Math.pow(12, 2));
+        }
+    }

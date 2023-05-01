c9ec13321459e62009d7134b70669955bcd4fdc5
Christian Lisangola <jochri3@gmail.com>
2023-05-01 09:35:13 +0300

    8.2.code

---
+++ 8.loops/8_2_code.java
@@ -0,0 +1,7 @@
+public class Program14LoopsIntro {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 12; i++) {
+            System.out.printf("The square of %d is %.0f\n", i, Math.pow(i, 2));
+        }
+    }
+}

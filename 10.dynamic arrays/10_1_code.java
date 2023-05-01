a680f147fd4cb8780cf8eebd78fa3525d054daea
Christian Lisangola <jochri3@gmail.com>
2023-05-01 11:03:37 +0300

    10.1.code

---
+++ 10.dynamic arrays/10_1_code.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Program18DynamicArrays {
+    public static void main(String[] args) {
+       ArrayList<Double> physicsGrades = new ArrayList<>();
+        ArrayList<String> fruits=new ArrayList<>();
+    }
+}
---
+++ 10.dynamic arrays/10_1_starter_code.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+ 
+ 
+public class Program18DynamicArrays {
+    public static void main(String[] args) {
+        int[] intsArray=new int[5];//0
+        double[] decimalsArray=new double[5];//0.0
+        boolean[] booleansArray=new boolean[5];//false
+        String[] stringsArray=new String[5];//null
+ 
+        System.out.println(Arrays.toString(intsArray));
+        System.out.println(Arrays.toString(decimalsArray));
+        System.out.println(Arrays.toString(booleansArray));
+        System.out.println(Arrays.toString(stringsArray));
+    }
+}
